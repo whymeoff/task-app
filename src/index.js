6f18ba73a1c75a5708db88ebf8e42a9e134a7772
Mark <50715288+whymeoff@users.noreply.github.com>
2020-01-11 16:20:15 +0200

    New routers path. Async/await refactoring.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,14 @@
 require('./db/mongoose')
 const express = require('express')
-const User = require('./models/user')
-const Task = require('./models/task')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
-
-app.post('/users', (req, res) => {
-    const user = new User(req.body);
-    user.save().then((data) => {
-        res.send(data);
-    }).catch((err) => {
-        res.status(400).send(err)
-    })
-})
-
-app.post('/tasks', (req, res) => {
-    const task = new Task(req.body);
-    task.save().then((data) => {
-        res.send(data);
-    }).catch((err) => {
-        res.status(400).send(err)
-    })
-})
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
