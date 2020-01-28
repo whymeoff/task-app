b4ab1a15375a63e9f99f699b114c8834bfcca296
Mark <50715288+whymeoff@users.noreply.github.com>
2020-01-28 19:50:51 +0200

    Authentication(pass crypt, login and etc)

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -21,11 +24,12 @@ const User = mongoose.model('User', {
         required: true,
         trim: true,
         lowercase: true,
+        unique: true,
         validate(value) {
             if (!validator.isEmail(value)) {
                 throw new Error('Your email is not right!')
             }
-        }
+        },
     },
     password: {
         type: String,
@@ -37,7 +41,76 @@ const User = mongoose.model('User', {
             }
         },
         trim: true
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
+
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'author'
+})
+
+userSchema.methods.generateAuthToken = async function() {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'someSecretValue')
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
+}
+
+userSchema.methods.toJSON = function() {
+    const user = this
+    const userObj = user.toObject()
+
+    delete userObj.password
+    delete userObj.tokens
+
+    return userObj
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+// Hash the plain text password
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+userSchema.pre('remove', async function (next) {
+    const user = this
+
+    await Task.deleteMany({ author: user._id })
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
