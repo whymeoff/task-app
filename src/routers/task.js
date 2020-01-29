58958f4465404d00cc2bdf16376b063730d911d1
Mark <50715288+whymeoff@users.noreply.github.com>
2020-01-29 19:28:13 +0200

    Sotring, pagination

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,10 +18,33 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+//GET /tasks?completed=true
+//GET /tasks?limit=10&skip=0
+//GET /tasks?sortBy=createdAd_asc || _desc
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+    let sortMethod
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true' 
+    }
+
+    if (req.query.sortBy) {
+        sortMethod = (req.query.sortBy.split('_')[1] === 'desc') ? -1 : 1
+    }
+    
     try {
-        //const tasks = await Task.find({ author: req.user._id })
-        await req.user.populate('tasks').execPopulate()
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort: {
+                    createdAt: sortMethod
+                }
+            }
+        }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()
